f4cd9cee2418274379f37fe27e888414b9926f77
StevenDeZalia <26663790+StevenDeZalia@users.noreply.github.com>
2017-10-25 19:00:06 -0400

    Update install_teamspeak3-server-ports.sh

--- install_teamspeak3-server-ports.sh
+++ install_teamspeak3-server-ports.sh
@@ -3,42 +3,42 @@
 #Synopsis:				Install TeamSpeak 3 Server from Ports
 #
 #Description:			Updates ports
-#						Installs TeamSpeak3 Server from ports.
-#						Adds teamspeak_enable="YES" to /etc/rc.conf
-#						Stop, start, and check status of TeamSpeak service
-#						Displays ServerAdmin privilege key
+#									Installs TeamSpeak3 Server from ports.
+#									Adds teamspeak_enable="YES" to /etc/rc.conf
+#									Stop, start, and check status of TeamSpeak service
+#									Displays ServerAdmin privilege key
 #
 #Notes:
 #		File Name:		Install_TeamSpeak3_Server_Ports_1_0.sh
-#		Version:		1.00
-#		Author:			Steven J. DeZalia
-#		Contact:		https://github.com/StevenDeZalia/
+#		Version:			1.00
+#		Author:				Steven J. DeZalia
+#		Contact:			https://github.com/StevenDeZalia/
 #		Prerequisite:	FreeBSD OS Installed with an active internet connection, built and tested on 10.3-Release-p8.
-#							As of this Writing 11+ breaks the TeamSpeak3 Server.
-#		Date:			09/23/2016
+#										As of this Writing 11+ breaks the TeamSpeak3 Server.
+#		Date:					09/23/2016
 #		Copyright:		Copyright (c) 2016, Steven J. DeZalia
-#						All rights reserved.
+#									All rights reserved.
 #
-#						Redistribution and use in source and binary forms, with or without
-#						modification, are permitted provided that the following conditions are met:
-#						    * Redistributions of source code must retain the above copyright
-#						      notice, this list of conditions and the following disclaimer.
-#						    * Redistributions in binary form must reproduce the above copyright
-#						      notice, this list of conditions and the following disclaimer in the
-#						      documentation and/or other materials provided with the distribution.
-#						    * Neither the name of the Steven J DeZalia nor the names of its contributors 
-#						      may be used to endorse or promote products derived from this software without 
-#							  specific prior written permission.
+#									Redistribution and use in source and binary forms, with or without
+#									modification, are permitted provided that the following conditions are met:
+#						    			* Redistributions of source code must retain the above copyright
+#						      			notice, this list of conditions and the following disclaimer.
+#						    			* Redistributions in binary form must reproduce the above copyright
+#						      			notice, this list of conditions and the following disclaimer in the
+#						      			documentation and/or other materials provided with the distribution.
+#						    			* Neither the name of the Steven J DeZalia nor the names of its contributors 
+#						      			may be used to endorse or promote products derived from this software without 
+#							  				specific prior written permission.
 #
-#						THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-#						ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#						WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#						DISCLAIMED. IN NO EVENT SHALL Steven J DeZalia DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-#						EXEMPLARY, OR CONSEQUENTIAL DAMAGES BE LIABLE FOR ANY(INCLUDING, BUT NOT LIMITED TO, 
-#						PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-#						INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-#						LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-#						OF THIS	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#									THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#									ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#									WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#									DISCLAIMED. IN NO EVENT SHALL Steven J DeZalia DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+#									EXEMPLARY, OR CONSEQUENTIAL DAMAGES BE LIABLE FOR ANY(INCLUDING, BUT NOT LIMITED TO, 
+#									PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+#									INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+#									LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+#									OF THIS	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #		
 #Link:					
 #
@@ -49,17 +49,18 @@
 #               this will be searched and displayed at the end of this script. If migrating from another instance of 
 #               TeamSpeak3 place files into /var/db/teamspeak, Then stop, start the server as notated below.
 #
-#						Permissions listed below:
+#								Permissions listed below:
 #
-#							ls -l /var/db/teamspeak/
-#							total 1512
-#							drwx------	4	teamspeak	teamspeak	512		Oct 23	2012	files/
-#							-rw-r--r--	1	teamspeak	teamspeak	1		Jul 13	2012	query_ip_blacklist.txt
-#							-rw-r--r--	1	teamspeak	teamspeak	10		Jul 13	2012	query_ip_whitelist.txt
-#							-rw-------	1	root		teamspeak	5		Aug 31	23:47	teamspeak_server.pid
-#							-rw-r--r--	1	teamspeak	teamspeak	416768	Sep 9	14:36	ts3server.sqlitedb
-#							-rw-r--r--	1	teamspeak	teamspeak	32768	Sep 11	00:36	ts3server.sqlitedb-shm
-#							-rw-r--r--	1	teamspeak	teamspeak	1048032	Sep 11	00:34	ts3server.sqlitedb-wal
+#								ls -l /var/db/teamspeak/
+#								total 1512
+#								drwx------	4	teamspeak	teamspeak	512		Oct 23	2012	files/
+#								-rw-r--r--	1	teamspeak	teamspeak	1		Jul 13	2012	query_ip_blacklist.txt
+#								-rw-r--r--	1	teamspeak	teamspeak	10		Jul 13	2012	query_ip_whitelist.txt
+#								-rw-------	1	root		teamspeak	5		Aug 31	23:47	teamspeak_server.pid
+#								-rw-r--r--	1	teamspeak	teamspeak	416768	Sep 9	14:36	ts3server.sqlitedb
+#								-rw-r--r--	1	teamspeak	teamspeak	32768	Sep 11	00:36	ts3server.sqlitedb-shm
+#								-rw-r--r--	1	teamspeak	teamspeak	1048032	Sep 11	00:34	ts3server.sqlitedb-wal
+
 
 #
 #Updates for FreeBSD, Ports, and PKG
